0881b287586074c517084f7b9de20a90d48eb8c6
Nandhakumarv10 <nandhakumar652001@gmail.com>
2025-02-20 15:49:50 +0530

    fixing the tailwind issue and now work on header

--- src/app/(consumer)/layout.tsx
+++ src/app/(consumer)/layout.tsx
@@ -1,18 +1,59 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 
 export default function ConsumerLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
-  <>
-    <Navbar />
-    {children}
-  </>;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
 }
 
 function Navbar() {
   return (
-    <header className="flex h-12 shadow bg-amber-200 z-10">
-      <nav className="flex gap-4 container"></nav>
+    <header className="flex h-12 shadow bg-white text-black items-center z-10">
+      <nav className="flex gap-4  w-full p-2 ">
+        <Link className="mr-auto text-lg hover:underline px-2" href="/">
+          Course application
+        </Link>
+
+        <Suspense>
+          <SignedIn>
+            <Link
+              href="/course"
+              className="hover:bg-accent/10 flex items-center px-2"
+            >
+              My Courses
+            </Link>
+            <Link href="/purchase" className="flex items-center px-2">
+              Purchase History
+            </Link>
+            <div className="size-8 self-center">
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: { width: "100%", height: "100%" },
+                  },
+                }}
+              />
+            </div>
+          </SignedIn>
+        </Suspense>
+        <Suspense>
+          <SignedOut>
+            <div>
+              <button className="p-3 bg-black rounded-3xl text-white">
+                <Link href="/sign-in">sign in</Link>
+              </button>
+            </div>
+          </SignedOut>
+        </Suspense>
+      </nav>
     </header>
   );
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",  // For App Router
+      "./pages/**/*.{js,ts,jsx,tsx}", // For Pages Router
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
